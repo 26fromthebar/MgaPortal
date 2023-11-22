fd0460b1775b1c4bd6cd86093dbbd239736835f8
Kostas Christou <christou@exeo.gr>
2023-11-22 19:14:32 +0200

    added basic components-empty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ApplicationsComponent } from './components/applications/applications.component';
+import { CollectionsComponent } from './components/collections/collections.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ItemComponent } from './components/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'collections', component: CollectionsComponent },
+  { path: 'collections/:uuid', component: ItemComponent },
+  { path: 'applications', component: ApplicationsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'collections', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ApplicationsComponent } from './components/applications/applications.component';
+import { CollectionsComponent } from './components/collections/collections.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BackToTopComponent } from './shared/back-to-top/back-to-top.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ApplicationsComponent,
+    CollectionsComponent,
+    PageNotFoundComponent,
+    BackToTopComponent,
+    FooterComponent,
+    HeaderComponent,
+    PaginationComponent,
+    ItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
